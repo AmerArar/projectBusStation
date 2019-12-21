8e308a44d6f6d130cde21b78e392014780ac8d56
Amer Abu Arar <amer.computer@gmail.com>
2019-12-21 17:10:34 +0200

    Update BusLines.h

--- BusStation/BusLines.h
+++ BusStation/BusLines.h
@@ -1,17 +1,21 @@
 #include <string>
+#include <vector>
+
 using namespace std;
 
 class BusLines
 {
 public:
-	BusLines();
+
+	BusLines(vector<vector<string> >& values);
 	~BusLines();
-	void setBusLines(string IdBusLines);
+	void setBusLines(string t);
 	string getBusLines();
 
 
 private:
 	string IdBusLines;
+	vector<string> visited;
 
 	
 };
