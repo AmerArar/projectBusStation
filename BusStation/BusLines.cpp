0ac5f363f563ae46b9c932d1a5b03875cbc7f40b
Amer Abu Arar <amer.computer@gmail.com>
2019-12-21 17:12:28 +0200

    Update BusLines.cpp
    view the list of all lines

--- BusStation/BusLines.cpp
+++ BusStation/BusLines.cpp
@@ -1,15 +1,46 @@
+#include <iostream>
+#include <string>
 #include "BusLines.h"
+#include <vector>
+
 using namespace std;
 
 
-BusLines::BusLines()
+
+BusLines::BusLines(vector<vector<string> >& values)
 {
-	//to do
+    int i = 0;
+    int m = 0;
+    string t = { "LineNum" };
+    //LineNum
+    ////
+    for (i;i < values[0].size();i++)
+    {
+        if (values[0][i] == t)
+            m = i;
+    }
+    //////////////
+   
+    i = m;
+    int c = 0;
+
+    for (int j = 0; j < values.size(); j++) {
+            for (c = 0; c < visited.size(); c++)
+                    if(visited[c] == values[j][i])
+                        m = 1;
+            if (m != 1)
+            {
+                visited.push_back(values[j][i]);
+                cout << values[j][i] << endl;
+            }
+            m = 0;
+    }
+    
 }
 
-void BusLines::setBusLines(string IdBusLines)
+void BusLines::setBusLines(string t)
 {
-	this->IdBusLines = IdBusLines;
+  
 }
 
 string BusLines::getBusLines()
@@ -18,4 +49,3 @@ string BusLines::getBusLines()
 }
 
 BusLines::~BusLines(){}
-
