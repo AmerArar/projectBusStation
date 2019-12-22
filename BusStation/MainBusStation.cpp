779bf08b9d585ce04d440c869d8ade22d2ef6e56
Amer Abu Arar <amer.computer@gmail.com>
2019-12-23 00:25:17 +0200

    Update file
    Update file

--- BusStation/MainBusStation.cpp
+++ BusStation/MainBusStation.cpp
@@ -8,6 +8,9 @@
 #include "stdio.h"
 #include <time.h>
 #include "BusLines.h"
+#include "BusStation.h"
+#include "Times.h"
+#include "City.h"
 #include <vector>
 
 using namespace std;
@@ -18,6 +21,7 @@ void choiceReservation() {
     cout << "Make a choice:" << endl;
     cout << "Press 1 to view the list of all lines:" << endl;
     cout << "Press 2 to view the list of all lines by region:" << endl;
+    cout << "Press 3 to view the list of all stations in a particular city:" << endl;
     cout << "Press Another number to cancel:" << endl;
     cout << "---------------------------------------" << endl;
 }
@@ -27,15 +31,16 @@ int main()
 {
     vector<vector<string> > values;
     vector<string> valueline;
-    vector<string> visited;
 
-    int x, y;
+    int x;
+    try {
         ifstream fin("C:\\BootCamp\\busStation\\suprBus.csv");
+        if (!fin)
+            throw FALSE;
         string item;
         for (string line; getline(fin, line); )
         {
             istringstream in(line);
-
 
             while (getline(in, item, ','))
             {
@@ -50,10 +55,32 @@ int main()
         choiceReservation();
 
         cin >> x;
+        string LineNum = { "LineNum" };
+        string AreaCode = { "AreaCode" };
+        string CityCode = { "240" };
 
         if (x == 1) {
             cout << "list of all lines:" << endl;
             BusLines L1(values);
         }
-        
- }
+        else
+        {
+            if (x == 2) {
+                cout << "list of all lines by region:" << endl;
+                BusLines L2(values, AreaCode);
+            }
+            else
+                if (x == 3) {
+                    cout << " list of all stations in a particular city:";
+                    BusStation S2(values, CityCode);
+                }
+
+
+        }
+
+    }
+    catch (...)
+    {
+        cout << "\n can't open file!!!.";
+    }
+}
