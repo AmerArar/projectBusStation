9c83e73279680f263c1e513756d32b1e45db87d1
Amer Abu Arar <amer.computer@gmail.com>
2019-12-23 00:20:23 +0200

    Update file

--- BusStation/BusLines.cpp
+++ BusStation/BusLines.cpp
@@ -5,47 +5,128 @@
 
 using namespace std;
 
-
-
-BusLines::BusLines(vector<vector<string> >& values)
+BusLines::BusLines(vector<vector<string>>& values)
 {
+
+    setBusLines(values);
+    visited.clear();
+    printLineNum();
+
+}
+
+
+BusLines::BusLines(vector<vector<string>>& values, string AreaCode)
+{
+
+    setBusLines(values);
+    visited.clear();
+
+    setRegions(values, AreaCode);
+    visited.clear();
+
     int i = 0;
-    int m = 0;
-    string t = { "LineNum" };
-    //LineNum
-    ////
+    int Areaid = 0;
     for (i;i < values[0].size();i++)
-    {
-        if (values[0][i] == t)
-            m = i;
+        if (values[0][i] == AreaCode)
+            Areaid = i;
+
+    i = 0;
+    int BLines = 0;
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == "LineNum")
+            BLines = i;
+
+    int c2 = 0;
+    for (int j = 0; j < vAreaCode.size(); j++) {
+        cout << endl << "______________________________________________________________________________________________________";
+        cout << endl << "Region Code  " << vAreaCode[j] << ":" << endl;
+        for (int i = 1; i < values.size(); i++) {
+            if (values[i][Areaid] == vAreaCode[j]) {
+                for (int L = 0;L < vLineNum.size();L++)
+                    if (values[i][BLines] == vLineNum[L]) {
+                        for (int v = 0; v < visited.size(); v++)
+                            if (visited[v] == vLineNum[L])
+                                c2 = 1;
+                        if (c2 != 1)
+                        {
+                            visited.push_back(vLineNum[L]);
+                        }
+                        c2 = 0;
+                    }
+            }
+        }
+        for (int i = 0; i < visited.size(); i++)
+            cout << visited[i] << " \t ";
+        visited.clear();
     }
-    //////////////
-   
-    i = m;
+
+}
+
+
+
+
+void BusLines::setBusLines(vector<vector<string> >& values)
+{
+    string LineNum = { "LineNum" };
+    int i = 0;
+    int BLines = 0;
     int c = 0;
 
-    for (int j = 0; j < values.size(); j++) {
-            for (c = 0; c < visited.size(); c++)
-                    if(visited[c] == values[j][i])
-                        m = 1;
-            if (m != 1)
-            {
-                visited.push_back(values[j][i]);
-                cout << values[j][i] << endl;
-            }
-            m = 0;
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == "LineNum")
+            BLines = i;
+
+    for (int j = 1; j < values.size(); j++) {
+        for (i = 0; i < visited.size(); i++)
+            if (visited[i] == values[j][BLines])
+                c = 1;
+        if (c != 1)
+        {
+            visited.push_back(values[j][BLines]);
+            vLineNum.push_back(values[j][BLines]);
+        }
+        c = 0;
     }
-    
+
+
 }
 
-void BusLines::setBusLines(string t)
+
+
+void BusLines::setRegions(vector<vector<string> >& values, string AreaCode)
 {
-  
+    int i = 0;
+    int Areaid = 0;
+    int c = 0;
+
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == AreaCode)
+            Areaid = i;
+
+    for (int j = 1; j < values.size(); j++) {
+        for (i = 0; i < visited.size(); i++)
+            if (visited[i] == values[j][Areaid])
+                c = 1;
+        if (c != 1)
+        {
+            visited.push_back(values[j][Areaid]);
+            vAreaCode.push_back(values[j][Areaid]);
+        }
+        c = 0;
+    }
+    visited.clear();
+
 }
 
-string BusLines::getBusLines()
+void BusLines::printLineNum()
 {
-	return IdBusLines;
+
+    for (int i = 0; i < vLineNum.size(); i++)
+        cout << vLineNum[i] << endl;
 }
 
-BusLines::~BusLines(){}
+BusLines::~BusLines() {
+    visited.clear();
+    vLineNum.clear();
+    vAreaCode.clear();
+}
