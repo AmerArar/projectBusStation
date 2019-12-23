5b2e22ce407c2f99ecd72a06dd321cd640823ea6
Amer Abu Arar <amer.computer@gmail.com>
2019-12-23 23:44:59 +0200

    Update Main
    Update Main

--- BusStation/MainBusStation.cpp
+++ BusStation/MainBusStation.cpp
@@ -22,6 +22,8 @@ void choiceReservation() {
     cout << "Press 1 to view the list of all lines:" << endl;
     cout << "Press 2 to view the list of all lines by region:" << endl;
     cout << "Press 3 to view the list of all stations in a particular city:" << endl;
+    cout << "Press 4 to view a list of all cities that can be reached directly from a particular station:"<<endl;
+    cout << "Press 5 to Back to menu" << endl;
     cout << "Press Another number to cancel:" << endl;
     cout << "---------------------------------------" << endl;
 }
@@ -32,7 +34,7 @@ int main()
     vector<vector<string> > values;
     vector<string> valueline;
 
-    int x;
+    int x=0;
     try {
         ifstream fin("C:\\BootCamp\\busStation\\suprBus.csv");
         if (!fin)
@@ -52,35 +54,52 @@ int main()
         }
 
         fin.close();
-        choiceReservation();
 
-        cin >> x;
-        string LineNum = { "LineNum" };
-        string AreaCode = { "AreaCode" };
-        string CityCode = { "240" };
 
-        if (x == 1) {
-            cout << "list of all lines:" << endl;
-            BusLines L1(values);
-        }
-        else
+        while (x < 6)
         {
-            if (x == 2) {
-                cout << "list of all lines by region:" << endl;
-                BusLines L2(values, AreaCode);
+
+            choiceReservation();
+
+            cin >> x;
+
+            string LineNum = { "LineNum" };
+            string AreaCode = { "AreaCode" };
+            string CityCode = { "240" };
+            string ParticularStationCode = { "55557" };
+            string s = s;
+            if (x == 1) {
+                cout << "list of all lines:" << endl;
+                BusLines L1(values);
             }
             else
-                if (x == 3) {
-                    cout << " list of all stations in a particular city:";
-                    BusStation S2(values, CityCode);
+                if (x == 2) {
+                    cout << "list of all lines by region:" << endl;
+                    BusLines L2(values, AreaCode);
                 }
-
-
+                else
+                    if (x == 3) {
+                        cout << " list of all stations in a particular city:";
+                        BusStation S1(values, CityCode);
+                    }
+                    else
+                        if (x == 4) {
+                            cout << " list of all cities that can be reached directly from a particular station:";
+                            BusStation S2(values, ParticularStationCode, s);
+                        }
+                        else
+                            if (x == 5)
+                                cout << "Back to menu" << endl;
+                            else
+                            {
+                                break;
+                            }
+            
         }
 
     }
-    catch (...)
+    catch (BOOL fin)
     {
-        cout << "\n can't open file!!!.";
+        cout << "\n ** can't open file!!! **";
     }
 }
