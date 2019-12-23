dfb57a60e555db421114a353bda89cf55fe8bf15
Amer Abu Arar <amer.computer@gmail.com>
2019-12-23 23:42:22 +0200

    Update file
    Update file

--- BusStation/BusStation.cpp
+++ BusStation/BusStation.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 #include "BusStation.h"
+#include "BusLines.h"
 #include <vector>
 
 using namespace std;
+BusStation::BusStation(vector<vector<string>>& values, string ParticularStationCode, string s)
+{
+    setBusDirectStation(values, ParticularStationCode);
+        visitedStation.clear();
+}
 
 BusStation::BusStation(vector<vector<string>>& values, string CodeOfCity)
 {
@@ -26,7 +32,7 @@ void BusStation::setBusStation(vector<vector<string>>& values, string CodeOfCity
     i = 0;
     int StationCode = 0;
     for (i;i < values[0].size();i++)
-        if (values[0][i] == "StationCode")//lin
+        if (values[0][i] == "StationCode")
             StationCode = i;
 
     int c2 = 0;
@@ -52,8 +58,65 @@ void BusStation::setBusStation(vector<vector<string>>& values, string CodeOfCity
 
 }
 
+void BusStation::setBusDirectStation(vector<vector<string>>& values, string ParticularStationCode)
+{   
+    visitedStation.clear();
+    string particularStationCode = ParticularStationCode;
+
+    int i = 0;
+    int StationCode = 0;
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == "StationCode")
+            StationCode = i;
+
+    i = 0;
+    int BLines = 0;
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == "LineNum")
+            BLines = i;
+
+    i = 0;
+    int BusCityCode = 0;
+    for (i;i < values[0].size();i++)
+        if (values[0][i] == "CityCode")
+            BusCityCode = i;
+
+    string BLinesStation;
+    string BusCityCodeStation;
+    int c2 = 0;
+
+    int v = 0;
+    for (i = 1; i < values.size(); i++) 
+        if (values[i][StationCode] == particularStationCode) {
+            vLineNumStation.push_back(values[i][BLines]);
+        }
+
+    for (int j = 1; j < values.size(); j++)
+        for (v = 0; v < vLineNumStation.size(); v++)
+            if (vLineNumStation[v] == values[j][BLines]){
+                    for (v = 0; v < visitedStation.size(); v++)
+                        if (visitedStation[v] == values[j][BusCityCode])
+                            c2 = 1;
+                    if (c2 != 1)
+                    {
+                        visitedStation.push_back(values[j][BusCityCode]);
+                    }
+                    c2 = 0;
+                        
+            }
+          
+
+    cout << particularStationCode << endl;
+    for (i = 0; i < visitedStation.size(); i++)
+        cout << visitedStation[i] << " \t ";
+    cout << endl << "______________________________________________________________________________________________________";
+    visitedStation.clear();
+   
+}
+
 
 BusStation::~BusStation()
 {
     visitedStation.clear();
+    vLineNumStation.clear();
 }
